768b00dde7a4480291350cbfba8c7a0f985b7675
Hank <45723675+HankTech@users.noreply.github.com>
2019-11-14 18:13:43 -0400

    Build

---
+++ precache-manifest.30a5105f321613d4f8a9943631770873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37eccc16a414f98869b525ae39186d78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f8facb33caeffb2910c",
+    "url": "/static/css/main.91754f63.chunk.css"
+  },
+  {
+    "revision": "b016be55812eb03fe1f1",
+    "url": "/static/js/2.3c3e6c87.chunk.js"
+  },
+  {
+    "revision": "4f8facb33caeffb2910c",
+    "url": "/static/js/main.c697ef70.chunk.js"
+  },
+  {
+    "revision": "35f1c29b915c40a4edde",
+    "url": "/static/js/runtime-main.aab5a7e3.js"
+  }
+]);
